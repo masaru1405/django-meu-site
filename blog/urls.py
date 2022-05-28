6b510bb7ff7f55a8afc201bb60dbd7a113648cee
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-05-28 08:27:14 -0300

    add update view

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
    path('', views.BlogListView.as_view(), name='blog.home'),
+   path('new/', views.BlogCreateView.as_view(), name='post.new'), #3h07 ver o problema relacionado ao uso de slug
    path('<slug:slug>/', views.BlogDetailView.as_view(), name='post.detail'), #vídeo 2h51m
+   path('<slug:slug>/edit/', views.BlogUpdateView.as_view(), name='post.edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -11,3 +12,14 @@ class BlogDetailView(DetailView):
    model = Post
    template_name = 'blog/detail.html'
    context_object_name = 'post'
+
+#2h52m
+class BlogCreateView(CreateView):
+   model = Post
+   template_name = 'blog/new.html'
+   fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+   model = Post
+   template_name = 'blog/edit.html'
+   fields = ('title', 'content')
