80632409f44ebd16da74847096b1134a648eac83
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-06-04 13:32:03 -0300

    allowed hosts

--- meu_site/settings.py
+++ meu_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://django-blog-kaio.herokuapp.com/']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
